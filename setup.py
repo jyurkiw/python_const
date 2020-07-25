dc1ee9c34b9e462e5d1fc89731fd3e330d9b4f39
Jeffrey Yurkiw <jyurkiw@yahoo.com>
2020-07-24 19:01:26 -0700

    Fixed problems with pip configuration

--- setup.py
+++ setup.py
@@ -4,16 +4,17 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="python_const",
+    name="pyconst",
     version="1.0",
     description="A small utility for creating constants containers.",
     url="https://github.com/jyurkiw/python_const",
     author="Jeffrey Yurkiw",
     long_description=long_description,
     long_description_content_type="text/markdown",
+    packages=["pyconst"],
     classifiers=[
-        "Programming Language :: Pythong :: 3",
-        "License :: MIT License",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
 )
